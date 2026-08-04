2d69a68eb47c7bad8db13b638689f2fe7197aa2a
haidang0811 <nguyenhaidang08112004@gmail.com>
2026-08-04 13:31:48 +0700

    hoan thanh slide 7

--- slide7/slide7.c
+++ slide7/slide7.c
@@ -1,4 +1,35 @@
 #include <stdio.h>
+#include <string.h>
+
 int main (){
+    char string1[20] = "tran QUang biNH"; //  = {'A','B','C'};
+    // char string[20] = " ";
+    printf("Chuoi 1: %s\n",string1);
+    printf("Chuoi 2: %s\n",strlwr(string1));
     
+    
+
+
+    // char ngheNghiep[20];
+    // printf("Nhap ho ten:");
+    // // scanf("%s",hoTen);
+    // gets(hoTen);
+    // // hoTen2 = hoTen;
+    // strcpy(hoTen2,hoTen);
+
+    // printf("Nhap nghe nghiep:");
+    // // scanf("%s",ngheNghiep);
+    // gets(ngheNghiep);
+
+    //  for(int i=0; i<strlen(hoTen2);i++){
+    //     printf("%C\n",hoTen[i]);
+    // }
+
+    // printf("\n\n");
+
+    // for(int i=0; i<strlen(ngheNghiep);i++){
+    //     printf("%C\n",ngheNghiep[i]);
+    // }
+
+    return 0;
 }
